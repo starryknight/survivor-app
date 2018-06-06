3af211733caf7bc1a66165aba51cbb86455a9559
starryknight <mrstanleykimani@gmail.com>
2018-06-06 19:34:06 -0400

    changed readme and added survivor schema

---
+++ db/schemas/survivorSchema.js
@@ -0,0 +1,20 @@
+const Schema = require('mongoose').Schema
+const medicalSchema = require('./medicalSchema')
+const equipmentSchema = require('./equipmentSchema')
+
+const survivorSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  description: String,
+  dateOfBirth: {
+    type: Date,
+    default: new Date()
+  },
+  diagnosis: String,
+  sex: String,
+  ssn:Number
+  medicals: [ medicalSchema ]
+  equipments: [equipmentSchema]
+})
+
+module.exports = survivorSchema
