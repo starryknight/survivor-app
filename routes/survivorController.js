8809209c153bd141d8caf99eb0a9fd962a8d861d
starryknight <mrstanleykimani@gmail.com>
2018-06-07 17:03:57 -0400

    Made changes to app.js

--- routes/survivorController.js
+++ routes/survivorController.js
@@ -7,3 +7,74 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+const express = require('express')
+const router = express.Router()
+const Survivor = require('../models/Survivor')
+
+/* GET survivor listing. */
+// localhost/survivor
+router.get('/', (req, res, next) => {
+
+  // Find all Survivors
+  Homework
+    .find()
+    .then((listOfSurvivors) => {
+
+      // Once you have all survivor, then render out index page homeworks is all
+      // pieces of data that match the Survivor Model
+      res.render('survivor/index', { listOfSurvivors: listOfSurvivors })
+    })
+    .catch((err) => res.send(err))
+
+})
+
+// NEW Route
+router.get('/new', (req, res) => {
+  res.render('survivor/new')
+})
+
+// CREATE Route
+router.post('/', (req, res) => {
+  const newSurvivor = req.body
+  Homework
+    .create(newSurvivor)
+    .then(() => {
+      res.redirect('/survivor')
+    })
+})
+
+// SHOW Route
+router.get('/:id', (req, res) => {
+  Survivor
+    .findById(req.params.id)
+    .then((survivorInstance) => {
+      res.render('survivor/show', { survivorInstance })
+    })
+})
+
+// EDIT Route
+router.get('/:id/edit', (req, res) => {
+  Survivor
+    .findById(req.params.id)
+    .then((survivorInstance) => {
+      res.render('survivor/edit', { survivorInstance: survivorInstance })
+    })
+})
+
+// UPDATE Route
+router.put('/:id', (req, res) => {
+  Survivor.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(() => {
+    res.redirect(`/survivor/${req.params.id}`)
+  })
+})
+
+// DELETE Route
+router.delete('/:id', (req, res) => {
+  Survivor.findByIdAndRemove(req.params.id)
+    .then(() => {
+      console.log('Successfully Delete ')
+      res.redirect('/survivor')
+    })
+})
+
+module.exports = router
