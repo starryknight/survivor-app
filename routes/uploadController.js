c9d063c0c6bb4a3e7791210e59e23b33f3e54728
starryknight <mrstanleykimani@gmail.com>
2018-06-11 12:00:43 -0400

    changed cards in index.hbs to be more responsive by adding column width

--- routes/uploadController.js
+++ routes/uploadController.js
@@ -2,11 +2,15 @@ var express = require('express');
 var router = express.Router();
 var multer  = require('multer')
 var upload = multer({ dest: 'public/uploads' })
+const Survivor = require('../models/Survivor')
+
+router.get('survivor/upload', (req, res) => {
+  res.render('survivor/upload')
+})
 
 router.post('/upload', upload.single(), function (req, res, next) {
   req.send(req.files)
- 
-  
 })
 
 
+module.exports = router
